648009378f2f276a166a50a12618eaecdb0f6eb8
trble91 <daleyaaron@gmail.com>
2025-06-19 07:04:50 -0400

    revert

--- src/app/Music/page.js
+++ src/app/Music/page.js
@@ -14,10 +14,10 @@ import EmailGateModal from '../Components/EmailGateModal.js';
 export default function Music() {
   const [selectedPost, setSelectedPost] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleEmailSubmit = () => {
-    // Send to your database or email service here
-    console.log("Captured email:", email);
-  };
+  // const handleEmailSubmit = () => {
+  //   // Send to your database or email service here
+  //   console.log("Captured email:", email);
+  // };
   const AlbumCards = [
     {
       title: "The Lost Baker",
